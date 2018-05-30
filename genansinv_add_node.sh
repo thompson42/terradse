42d02a99707f718b4119adec2510d271b41187b1
thompson42 <alexthompson@sitelabs.com>
2018-05-30 15:08:19 +1000

    Add node fixes...

--- genansinv_add_node.sh
+++ genansinv_add_node.sh
@@ -3,17 +3,19 @@
 TFSTATE_FILE=/tmp/tfshow_add_node.txt
 terraform show terraform_add_node/terraform.tfstate > $TFSTATE_FILE
 
-echo ">>>> Requires a CURRENT ACCURATE ansible/hosts file for the cluster to correctly generate Certificates. <<<<"
+echo ">>>> Requires a CURRENT 100% ACCURATE ansible/hosts file for the cluster to correctly generate Certificates. <<<<"
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-  echo "Error: script has less than 1 arguments:"
+  echo "Error: script has less than 2 arguments:"
   echo "Requires dc_name: the name of the DC you wish to add the node to"
-  echo "-- usage: genansinv_add_node.sh [<dc_name>]"
+  echo "Requires opsc_server_private_ip: the private ip-address of the OPSC server"
+  echo "-- usage: genansinv_add_node.sh [<dc_name>] [<opsc_server_private_ip>]"
   exit 1
 fi
 
 DC_NAME="$1"
+$opscSrvPrivateIp = "$2"
 
 public_ip=()
 private_ip=()
@@ -29,20 +31,31 @@ do
    fi
 done < $TFSTATE_FILE
 
-# add a line to the existing hosts file under the target DC area: [dc_name]
+# add a line to the existing hosts file under the target DC section: [<dc_name>]
 # test: may be a collection
 if [ "${#public_ip[@]}" -eq 0 ]; then
-     LINE_TO_INSERT="${private_ip[i]} private_ip=${private_ip[i]} private_dns=${private_dns[i]} seed=false dc=${DC_NAME} rack=RAC1 vnode=1 initial_token="
-     ADD_NODE_IP_ADDRESS="${private_ip[i]}"
+   DSE_LINE_TO_INSERT="${private_ip[i]} private_ip=${private_ip[i]} private_dns=${private_dns[i]} seed=false dc=${DC_NAME} rack=RAC1 vnode=1 initial_token="
+   ADD_NODE_IP_ADDRESS="${private_ip[i]}"
 else
-     LINE_TO_INSERT="${public_ip[i]} private_ip=${private_ip[i]} private_dns=${private_dns[i]} seed=false dc=${DC_NAME} rack=RAC1 vnode=1 initial_token="
-     ADD_NODE_IP_ADDRESS="${public_ip[i]}"
+   DSE_LINE_TO_INSERT="${public_ip[i]} private_ip=${private_ip[i]} private_dns=${private_dns[i]} seed=false dc=${DC_NAME} rack=RAC1 vnode=1 initial_token="
+   ADD_NODE_IP_ADDRESS="${public_ip[i]}"
 fi
 
 MATCH="[${DC_NAME}]"
 FILE="ansible/hosts"
 
-sed -i "s/$MATCH/$MATCH\n$LINE_TO_INSERT/" $FILE
+sed -i "s/$MATCH/$MATCH\n$DSE_LINE_TO_INSERT/" $FILE
+
+# add a line to the existing hosts file under the [datastax-agent] section
+if [ "${#public_ip[@]}" -eq 0 ]; then
+   OPSC_LINE_TO_INSERT="${private_ip[i]} private_ip=${private_ip[i]} private_dns=${private_dns[i]} opsc_srv_ip=$opscSrvPrivateIp" $DSE_ANSINV_FILE
+else
+   OPSC_LINE_TO_INSERT="${public_ip[i]} private_ip=${private_ip[i]} private_dns=${private_dns[i]} opsc_srv_ip=$opscSrvPrivateIp" $DSE_ANSINV_FILE
+fi
+
+MATCH="[datastax_agent]"
+
+sed -i "s/$MATCH/$MATCH\n$OPSC_LINE_TO_INSERT/" $FILE
 
 # replace a section at the end of the hosts file:
 #
--- runansi_add_node.sh
+++ runansi_add_node.sh
@@ -39,13 +39,25 @@ echo ">>>> Configure Spark security for New Node <<<<"
 echo
 echo ">>>> Setup New Node Spark Transport Encryption <<<<"
 echo
-ansible-playbook -i hosts spark_security.yml --private-key=~/.ssh/id_rsa_aws
+ansible-playbook -i hosts add_node_spark_security.yml --private-key=~/.ssh/id_rsa_aws
 echo
 
 echo
 echo ">>>> Activate New Node Spark Authentication <<<<"
 echo
-ansible-playbook -i hosts spark_authentication.yml --private-key=~/.ssh/id_rsa_aws
+ansible-playbook -i hosts add_node_spark_authentication.yml --private-key=~/.ssh/id_rsa_aws
+echo
+
+echo
+echo ">>>> Start Agent on New Node <<<<"
+echo
+ansible-playbook -i hosts add_node_agents_start.yml --private-key=~/.ssh/id_rsa_aws
+echo
+
+echo
+echo ">>>> Start DSE Enterprise on New Node <<<<"
+echo
+ansible-playbook -i hosts add_node_dse_start.yml --private-key=~/.ssh/id_rsa_aws
 echo
 
 cd ..
